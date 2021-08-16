77522ae43105125d80cec9f4ae6eb156d65835b3
Sujoy Ghosh <ghoshsujoy336@gmail.com>
2021-08-16 11:04:55 +0530

    more button working and minor changes

--- src/Components/AstronomyPicture/AstronomyPictures.jsx
+++ src/Components/AstronomyPicture/AstronomyPictures.jsx
@@ -11,20 +11,26 @@ import Accordion from "@material-ui/core/Accordion";
 import AccordionSummary from "@material-ui/core/AccordionSummary";
 import AccordionDetails from "@material-ui/core/AccordionDetails";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import CircularProgress from "@material-ui/core/CircularProgress";
 
 const useStyles = makeStyles(() => ({
   image: {
     width: "100%",
     objectFit: "contain",
   },
+  card: {
+    borderRadius: 10,
+    boxShadow: "0 8px 16px 0 rgba(0,0,0,0.08)",
+  },
 }));
 
 export default function AstronomyPictures() {
   const [pictures, setPictures] = useState([]);
+  const [startDate, setStartDate] = useState(null);
   const classes = useStyles();
 
-  const dateOfLastWeek = () => {
-    const initialDate = new Date();
+  const dateOfLastWeek = (date) => {
+    const initialDate = date;
     const finalDate = new Date(initialDate.getTime() - 7 * 24 * 60 * 60 * 1000);
     console.log("cd: ", initialDate);
     console.log("fd: ", finalDate);
@@ -43,13 +49,27 @@ export default function AstronomyPictures() {
       )
       .then((res) => {
         console.log("got images: ", res.data);
-        setPictures(res.data);
+        setPictures(res.data.reverse());
       })
       .catch((err) => console.log("got error: ", err.message));
   };
 
+  const fetchMore = () => {
+    const initialDate = dateOfLastWeek(startDate);
+    setStartDate(initialDate);
+    const start_date =
+      initialDate.getFullYear() +
+      "-" +
+      correctDigit(initialDate.getMonth() + 1) +
+      "-" +
+      correctDigit(initialDate.getDate());
+    console.log("start date: ", start_date);
+    getImages(start_date);
+  };
+
   useEffect(() => {
-    const initialDate = dateOfLastWeek();
+    const initialDate = dateOfLastWeek(new Date());
+    setStartDate(initialDate);
     const start_date =
       initialDate.getFullYear() +
       "-" +
@@ -59,6 +79,10 @@ export default function AstronomyPictures() {
     console.log("start date: ", start_date);
     getImages(start_date);
   }, []);
+
+  const seeImage = (url) => {
+    window.open(url);
+  };
 
   return (
     <Grid container>
@@ -74,16 +98,24 @@ export default function AstronomyPictures() {
         </Typography>
         <Grid container item spacing={2} style={{ marginTop: "2rem" }}>
           {pictures.length === 0 && (
-            <Typography variant="caption" color="textSecondary">
-              No images
-            </Typography>
+            <Grid container justifyContent="center">
+              <CircularProgress />
+            </Grid>
           )}
           {pictures.map((picture) => (
             <Grid item sm={4} xs={12} key={picture.date}>
-              <Card variant="outlined" style={{ borderRadius: 10 }}>
-                <img src={picture.url} alt="" className={classes.image} />
+              <Card className={classes.card}>
+                <img
+                  src={picture.url}
+                  alt=""
+                  className={classes.image}
+                  onClick={() => seeImage(picture.hdurl)}
+                />
                 <Typography variant="body1" style={{ marginLeft: 10 }}>
-                  {picture.date}
+                  {picture.date}{" "}
+                  <span style={{ fontSize: "small" }}>
+                    (click to see image in full resoulution)
+                  </span>
                 </Typography>
                 <Accordion style={{ border: "none" }}>
                   <AccordionSummary
@@ -108,6 +140,7 @@ export default function AstronomyPictures() {
           fullWidth
           style={{ marginTop: "1rem" }}
           disabled={pictures.length === 0 ? true : false}
+          onClick={() => fetchMore()}
         >
           more
         </Button>
